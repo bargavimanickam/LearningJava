360cb5f143e03c0e13efac1b4331690397fea058
bargavimanickam <41501720+bargavimanickam@users.noreply.github.com>
2019-07-16 14:57:17 +0530

    helloworld by returning

---
+++ bar2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class bar2 {
+ public static String bar()
+{
+  return "HELLO WORLD";
+}
+	public static void main(String[] args) {
+		System.out.println(bar());
+		
+  }
+}
