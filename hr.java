a19320d00806fa554dc5a894150d07ab48feccc6
bargavimanickam <41501720+bargavimanickam@users.noreply.github.com>
2019-07-16 14:58:20 +0530

    hello world by returning

---
+++ hr.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class hr {
+ public static String bar()
+{
+  return "HELLO WORLD";
+}
+	public static void main(String[] args) {
+		System.out.println(bar());
+		
+  }
+}
