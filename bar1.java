4511779f08b7a854adc6f21b13dd0f5d57b79f72
bargavimanickam <41501720+bargavimanickam@users.noreply.github.com>
2019-07-16 14:55:09 +0530

    helloworld by function

---
+++ bar1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class bar1 {
+ public static void bar()
+{
+  System.out.println("HELLO WORLD");
+}
+	public static void main(String[] args) {
+		bar();
+		
+  }
+}
